793549ceee31e3c44fb0923ca6ecfb8ea0efd1f5
deepakkushwaha <kushwahadeepak2911@gmail.com>
2026-07-19 22:43:53 +0530

    add login api

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,9 +2,21 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
-// import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 
-
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        return {accessToken, refreshToken}
+    }
+    catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access token and refresh token")
+    }
+}
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
@@ -50,6 +62,58 @@ const registerUser = asyncHandler( async (req, res) => {
         new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
-} )
+})
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res)=>{
+    //req body -> email, password
+    //validation - not empty
+    //check if user exists with email
+    //compare password with hashed password in db
+    //generate access token and refresh token
+    //save refresh token in db
+    //return response with access token and refresh token
+
+    const {email, password} = req.body
+     if(!email){
+        throw new ApiError(400, "Email is required")
+     }
+     const user = await User.findOne({email})
+     
+     if(!user){
+        throw new ApiError(404, "User not found")
+     }
+
+     const isPasswordValid =  await user.isPasswordCorrect(password)
+     if(!isPasswordValid){
+        throw new ApiError(401, "Invalid User Credentials")
+     }
+
+     const {accessToken , refreshToken} = await generateAccessTokenAndRefreshToken(user._id)
+      const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+      const options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 2 * 60 * 60 * 1000, // 2 hours
+      }
+      return res.status(200)
+      .cookie("refreshToken", refreshToken, options)
+      .cookie("accessToken", accessToken, options)
+      .json (new ApiResponse(200, {
+        user: loggedInUser, accessToken, refreshToken
+      }, "User logged in successfully"))
+
+})
+
+const logoutUser = asyncHandler(async (req, res)=>{
+    //get refresh token from cookies
+    //validate refresh token
+    //remove refresh token from db
+    //remove cookies
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,9 @@
+
+
+export const verifyJWT = asyncHandler(async (req, res, next)=>{
+    //get access token from cookies
+    //validate access token
+    //if valid, attach user to req object and call next()
+    //if invalid, return 401 unauthorized error
+    
+})
