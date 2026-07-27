7d8312ce02e3d6cc7a2fe4db9f9440e493157cee
deepakkushwaha <kushwahadeepak2911@gmail.com>
2026-07-27 18:08:17 -0400

    update captcha issue

--- src/controllers/captcha.controllers.js
+++ src/controllers/captcha.controllers.js
@@ -1,8 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import svgCaptcha from 'svg-captcha';
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { getCaptchaText, saveCaptcha } from "../utils/captchaStore.js";
+import { saveCaptcha } from "../utils/captchaStore.js";
 import { generateCaptchaAudio } from "../utils/generateCaptchaAudio.js";
 
 const getCaptcha = asyncHandler(async (req, res) => {
@@ -16,25 +15,16 @@ const getCaptcha = asyncHandler(async (req, res) => {
     })
 
     const captchaId = await saveCaptcha(captcha.text);
-    return res.status(200).json(new ApiResponse(200, "Captcha generated SUccessfully", { captchaId, image: captcha.data }));
+    const audioBuffer = await generateCaptchaAudio(captcha.text);
+    const audioBased64 = audioBuffer.toString("base64");
+
+    return res.status(200).json(
+        new ApiResponse(200, "captcha generate successfully", {
+            captchaId,
+            image: captcha.data,
+            audio: `data:audio/wav;base64,${audioBase64}`,
+        }),
+    )
 })
 
-const getCaptchaAudio = asyncHandler(async (req, res) => {
-    const { captchaId } = req.params;
-    const text = await getCaptchaText(captchaId);
-    if (!text) {
-        throw new ApiError(400, "Captcha expired or invalid, please refresh the captcha")
-    }
-
-    const audioBuffer = await generateCaptchaAudio(text);
-
-    res.set({
-        "Content-Type": "audio/wav",
-        "Content-Length": audioBuffer.length,
-        "Cache-Control": "no-store"
-    });
-
-    return res.status(200).send(audioBuffer)
-})
-
-export { getCaptcha, getCaptchaAudio }
+export { getCaptcha }
--- src/routes/captcha.routes.js
+++ src/routes/captcha.routes.js
@@ -1,10 +1,9 @@
 import{Router} from 'express';
-import {getCaptcha, getCaptchaAudio} from '../controllers/captcha.controllers.js'
+import {getCaptcha} from '../controllers/captcha.controllers.js'
 
 const router = Router();
 
 router.get("/generate", getCaptcha);
-router.get("/audio", getCaptchaAudio)
 
 export default router;
                  
